e2780d43ee8e755b3d5145ce4547fb2f212d23d5
adarshchhetri9 <chhetriadarsh912@gmail.com>
2023-01-21 19:44:39 +0530

    FormField and CreatePost

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const FormField = () => {
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
   return (
-    <div>FormField</div>
-  )
-}
+    <div>
+      <div className="flex items-center gap-2 mb-2 ">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
+        </label>
+      </div>
+    </div>
+  );
+};
 
-export default FormField
+export default FormField;
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,10 +6,53 @@ import { getRandomPrompt } from "../utils";
 import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [form, setForm] = useState({
-    
-  })
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+  const handleChange = (e) => {};
+  const handleSurpeiseMe = () => {};
+
+  return (
+    <section className="max-w-7xl mx-auto ">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+          Create of imaginative and visually stunning image through DALL-E AI
+          and share them with the community
+        </p>
+      </div>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="Adarsh Chhetri"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="Magic is on the way"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpeiseMe={handleSurpeiseMe}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePost;
