632377536c80b1635f20607ec51f27c1bda8089e
adarshchhetri9 <chhetriadarsh912@gmail.com>
2023-01-21 18:39:01 +0530

    assest update

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Form, useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
-}
+  const navigate = useNavigate()
+  const [form, setForm] = useState({
+    
+  })
+};
 
-export default CreatePost
+export default CreatePost;
