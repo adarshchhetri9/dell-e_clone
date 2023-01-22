081158528e57da90262a8b313dfe44ff5fb64a82
adarshchhetri9 <chhetriadarsh912@gmail.com>
2023-01-22 15:37:17 +0530

    till submit of create-post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,6 +16,8 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const generateImage = () => {};
+
   const handleSubmit = () => {};
   const handleChange = (e) => {};
   const handleSurpeiseMe = () => {};
@@ -49,6 +51,48 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpeiseMe={handleSurpeiseMe}
           />
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12  h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="mt-5 gap-5 flex ">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-green-600 font-medium rounded-md text-sm w-full sm:w-auto text-center px-5 py-2.5 "
+          >
+            {generatingImg ? "Generating..." : "Generate"}
+          </button>
+        </div>
+
+        <div className="mt-10 gap-5 flex ">
+          <p className="mt-2 ">Once you have created the image you want you can share it with the community </p>
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-violet-500 font-medium rounded-md text-sm w-full sm:w-auto text-center px-5 py-2.5 "
+          >
+            {generatingImg ? "Generating..." : "Submit"}
+          </button>
         </div>
       </form>
     </section>
